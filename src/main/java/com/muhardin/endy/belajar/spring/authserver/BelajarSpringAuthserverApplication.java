8895421cb50c3bfc247ec78611cb765b05e9283a
Endy Muhardin <endy.muhardin@gmail.com>
2021-03-25 10:22:51 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/spring/authserver/BelajarSpringAuthserverApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.spring.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringAuthserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringAuthserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muhardin/endy/belajar/spring/authserver/BelajarSpringAuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.spring.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringAuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
