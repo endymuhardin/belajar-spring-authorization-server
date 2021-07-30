4722386fac3e3b6932a1bca9996e5346e787d506
Endy Muhardin <endy.muhardin@gmail.com>
2021-07-30 15:10:42 +0700

    mengambil data authorities dari token JWT

--- resource-server/src/main/java/com/muhardin/endy/belajar/resourceserver/OauthResourceServerApplication.java
+++ resource-server/src/main/java/com/muhardin/endy/belajar/resourceserver/OauthResourceServerApplication.java
@@ -2,9 +2,24 @@ package com.muhardin.endy.belajar.resourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @SpringBootApplication
 public class OauthResourceServerApplication {
+
+	private static final String AUTHORITIES_CLAIM = "authorities";
+
+	@Bean
+	public JwtAuthenticationConverter jwtAuthenticationConverter() {
+		JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+		grantedAuthoritiesConverter.setAuthoritiesClaimName(AUTHORITIES_CLAIM);
+
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+		return jwtAuthenticationConverter;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OauthResourceServerApplication.class, args);
